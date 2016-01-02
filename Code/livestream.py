c64f5dfb259141186978ed6b57c7ae9c1d6915d6
Eagle220 <clement.jouault@orange.fr>
2016-01-03 00:25:28 +0100

    Finalisation livestream

--- Code/livestream.py
+++ Code/livestream.py
@@ -1,39 +1,47 @@
-from videostream import *
+
+
+import cv2
 import struct
 
 import socket
 
-host = ''
-port = 50000
-b = 5
-s = 1024
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class LiveStream(object):
 
-s.bind((host, port))
-s.listen(5)
-vs = VideoStream((640, 480), 100, -100, 75, 32).start()
-client, address = s.accept()
+    def __init__(self):
 
-try:
-    while 1:
+        self.host = ''
+        self.port = 50000
+        self.b = 5
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        f = vs.read()
-        while f == None:
-            f = vs.read()
-        f = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
-#        print(f)
-#        frame = f.flatten()
-        data = f.tostring()
-#        print(len(data))
-        msg = struct.pack('>I', len(data)) + data
+        self.s.bind((self.host, self.port))
+        self.s.listen(5)
+
+        self.client, self.address = self.s.accept()
+
+    def send(self, f):
+
+#        self.f = cv2.cvtColor(self.f, cv2.COLOR_BGR2GRAY)
+    #        print(f)
+    #        frame = f.flatten()
+        self.data = self.f.tostring()
+    #        print(len(data))
+        self.msg = struct.pack('>I', len(self.data)) + self.data
         print('[INFO] Frame envoyee')
-        client.sendall(msg)
-        
-except KeyboardInterrupt:
-    print("\n[END] Arret clavier")
+        self.client.sendall(self.msg)
 
-finally:
-    s.close()
-    vs.stop()
-    
+    def close(self):
+        self.s.close()
+
+#if __name__ == '__main__':
+#    try:
+#
+#
+#
+#    except KeyboardInterrupt:
+#        print("\n[END] Arret clavier")
+#x
+#    finally:
+#        s.close()
+#        vs.stop()
