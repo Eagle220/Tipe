a797399da7a3adfab102af41c8c7f763d19baae3
Eagle220 <clement.jouault@orange.fr>
2016-03-21 20:50:38 +0100

    Modification globales du code

---
+++ Code/Traitement.py
@@ -0,0 +1,19 @@
+from threading import Thread
+import cv2
+
+class Traitement(object):
+    """Classe de traitement des frames"""
+    def __init__(self, name):
+        self.name = name
+
+    def traitement(self):
+
+        return
+
+
+    def push_frame(self, frame):
+        self.frame = frame
+
+    def start(self):
+        return
+        
