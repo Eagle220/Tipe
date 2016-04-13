17d4aedfcc34bf72a6cba3e2b283fd8c81e607cf
Eagle220 <clement.jouault@orange.fr>
2016-04-13 20:53:40 +0200

    encode

--- Code/livestream.py
+++ Code/livestream.py
@@ -1,4 +1,4 @@
-
+# coding:utf8
 
 import cv2
 import struct
--- Code/videostream.py
+++ Code/videostream.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python
 # coding:utf8
+#!/usr/bin/env python
+
 
 from picamera import PiCamera
 from picamera.array import PiRGBArray
